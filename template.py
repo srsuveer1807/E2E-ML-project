ef007b2d5bec30250b6a453f7825f77860c348d7
Suveer SR <srsuveer9896@gmail.com>
2025-07-15 19:44:58 +0530

    folder structure created

---
+++ template.py
@@ -0,0 +1,49 @@
+import os
+from pathlib import Path
+import logging
+
+logging.basicConfig(level=logging.INFO,format='[%(asctime)s]:%(message)s:')
+
+project_name='mlProject'
+
+file_list=[
+    ".github/workflows/.gitkeep",
+    f"src/{project_name}/_init__.py",
+    f"src/{project_name}/components/_init__.py",
+    f"src/{project_name}/utils/_init__.py",
+    f"src/{project_name}/utils/common.py",
+    f"src/{project_name}/config/_init_.py",
+    f"src/{project_name}/config/configuration.py",
+    f"src/{project_name}/pipeline/_init__.py",
+    f"src/{project_name}/entity/_init__.py",
+    f"src/{project_name}/entity/config_entity.py",
+    f'src/{project_name}/constants/_init__.py',
+    "config/config.yaml",
+    # "dvc.yaml",
+    "params.yaml",
+    "schema.yaml",
+    "main.py",
+    "app.py",
+    "Dockerfile",
+    "requirements.txt",
+    "setup.py",
+    "research/trials.ipynb",
+    "templates/index.html",
+    'test.py'
+]
+
+for filepath in file_list:
+    filepath=Path(filepath)
+    filedir,filename=os.path.split(filepath)
+
+    if filedir!="":
+        os.makedirs(filedir,exist_ok=True)
+        logging.info(f'Creating Directory:{filedir} for the file: {filename}')
+    
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath)==0):
+        with open(filepath,'w') as f:
+            pass
+            logging.info(f"Creating empty file:{filepath}")
+    
+    else:
+        logging.info(f'{filename} already exists')

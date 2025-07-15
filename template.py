67dac0a28cdcfca5ca084356b7d4dc921351d487
Suveer SR <srsuveer9896@gmail.com>
2025-07-15 22:35:54 +0530

    exception,logger and utils added

--- template.py
+++ template.py
@@ -2,22 +2,22 @@ import os
 from pathlib import Path
 import logging
 
-logging.basicConfig(level=logging.INFO,format='[%(asctime)s]:%(message)s:')
+logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s')
 
-project_name='mlProject'
+project_name = 'mlProject'
 
-file_list=[
+file_list = [
     ".github/workflows/.gitkeep",
-    f"src/{project_name}/_init__.py",
-    f"src/{project_name}/components/_init__.py",
-    f"src/{project_name}/utils/_init__.py",
+    f"src/{project_name}/__init__.py",
+    f"src/{project_name}/components/__init__.py",
+    f"src/{project_name}/utils/__init__.py",
     f"src/{project_name}/utils/common.py",
-    f"src/{project_name}/config/_init_.py",
+    f"src/{project_name}/config/__init__.py",
     f"src/{project_name}/config/configuration.py",
-    f"src/{project_name}/pipeline/_init__.py",
-    f"src/{project_name}/entity/_init__.py",
+    f"src/{project_name}/pipeline/__init__.py",
+    f"src/{project_name}/entity/__init__.py",
     f"src/{project_name}/entity/config_entity.py",
-    f'src/{project_name}/constants/_init__.py',
+    f"src/{project_name}/constants/__init__.py",
     "config/config.yaml",
     # "dvc.yaml",
     "params.yaml",
@@ -29,21 +29,20 @@ file_list=[
     "setup.py",
     "research/trials.ipynb",
     "templates/index.html",
-    'test.py'
+    "test.py"
 ]
 
 for filepath in file_list:
-    filepath=Path(filepath)
-    filedir,filename=os.path.split(filepath)
+    filepath = Path(filepath)
+    filedir, filename = os.path.split(filepath)
 
-    if filedir!="":
-        os.makedirs(filedir,exist_ok=True)
-        logging.info(f'Creating Directory:{filedir} for the file: {filename}')
+    if filedir != "":
+        os.makedirs(filedir, exist_ok=True)
+        logging.info(f"Creating directory: {filedir} for the file: {filename}")
     
-    if (not os.path.exists(filepath)) or (os.path.getsize(filepath)==0):
-        with open(filepath,'w') as f:
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
+        with open(filepath, 'w') as f:
             pass
-            logging.info(f"Creating empty file:{filepath}")
-    
+        logging.info(f"Creating empty file: {filepath}")
     else:
-        logging.info(f'{filename} already exists')
+        logging.info(f"{filename} already exists")
